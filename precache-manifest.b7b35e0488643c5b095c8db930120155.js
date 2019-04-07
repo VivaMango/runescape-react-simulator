0059a804c09592aded5c4aef6ec25b0f30fa0299
VivaMango <VivaMangoMail@gmail.com>
2019-04-06 23:07:27 -0400

    Updates

---
+++ precache-manifest.b7b35e0488643c5b095c8db930120155.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aaa476d5fa1f48496181",
+    "url": "/runescape-react-simulator/static/js/runtime~main.b524ad8e.js"
+  },
+  {
+    "revision": "c12a6523875399853e95",
+    "url": "/runescape-react-simulator/static/js/main.49cdce5e.chunk.js"
+  },
+  {
+    "revision": "664fcb3371389a6326ed",
+    "url": "/runescape-react-simulator/static/js/2.25312ee8.chunk.js"
+  },
+  {
+    "revision": "c12a6523875399853e95",
+    "url": "/runescape-react-simulator/static/css/main.9a265164.chunk.css"
+  },
+  {
+    "revision": "9c5f939f9bc4c7ee9111bcead8f0b44c",
+    "url": "/runescape-react-simulator/index.html"
+  }
+];
